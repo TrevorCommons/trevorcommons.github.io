81f5ad5707f20863342d0292db6a7c036c1d5434
TrevorCommons <commonstrevor@gmail.com>
2025-09-13 16:05:10 -0400

    working on about section html, css, and js/jquery

--- js/main.js
+++ js/main.js
@@ -1,18 +1,13 @@
 //main.js
-document.addEventListener("DOMContentLoaded", () => {
-  document.querySelectorAll('nav ul.menu a').forEach(link => {
-    link.addEventListener('click', function(event) {
-      event.preventDefault(); // Stop default behavior
 
-      const pageName = this.getAttribute('data-page');
 
-      // Use hash instead of pushState
-      window.location.hash = pageName;
+// about code
+$(document).ready(function() {
+  $(".accordion-header").click(function() {
+    // Close any open content except the one clicked
+    $(".accordion-content").not($(this).next()).slideUp();
 
-      document.getElementById("content").innerHTML = `
-        <h2>${pageName} Page</h2>
-        <p>Content for ${pageName} goes here.</p>
-      `;
-    });
+    // Toggle the clicked content
+    $(this).next().slideToggle();
   });
 });
